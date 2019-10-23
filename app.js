48cccedaf38c160db33112cb8a7852d755fcd2a4
marleen kovalik <marleen.kovalik@khk.ee>
2019-10-23 16:18:03 +0300

    andmetüübid

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-console.log("JS test");
+// andmetüübid
+
+// täisarvud
+console.log(10);
+console.log(-4);
+console.log(0);
+
+// reaalarvud - ujuvkomaarvud
+console.log(2.5);
+console.log(-7.23);
+console.log(-2.0);
+
+// sõned - string ehk tekst
+console.log('Hello world!');
+
+// tõeväärtused
+console.log(true); // tõene
+console.log(false); // väär
+
+// nimekiri - list []
+console.log([1, 2, 3, 4]);
+
+// objektid {}
+console.log({a : 1, b : 2});
+console.table({a : 1, b : 2});
+
+
+
