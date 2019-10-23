70f20c9aa96254323b8b4658bfd850158a7fcedd
marleen kovalik <marleen.kovalik@khk.ee>
2019-10-23 16:50:19 +0300

    muutujate initsialiseerimine, defineerimine, omistamine

--- app.js
+++ app.js
@@ -1,27 +1,43 @@
-// logid konsoolile
+// muutujad
 
-// logid
-console.log('123');
+// var
+// muutujate defineerimine
+var nimi = 'Marleen'; // defineerimine
+console.log(nimi);
+nimi = 'Katrin'; // uue väärtuse omistamine
+console.log(nimi);
 
-// tabel
-console.log({a : 1, b : 2});
+// muutujate inistialiseerimine
+var tervitus;
+console.log(tervitus);
+tervitus = 'Tere tulemast!';
+console.log(tervitus);
 
-// veateated
-console.error('See on error');
+// liitnimetused muutujatel
+var eesNimi = 'Marleen'; // Camel style
+var ees_nimi = 'Marleen'; // PHP style
+var EesNimi = 'Marleen'; // Pascal style
+var eesnimi = 'Marleen'; //
 
-// märkused - warning
-console.warn('see on warning');
+// let - muutuja
+let pereNimi = 'Kovalik';
+console.log(pereNimi);
+pereNimi = '???';
+console.log(pereNimi);
 
-// ajakulu kontroll
-console.time('test');
-    console.log('mingi tegevus');
-    console.log('mingi tegevus');
-    console.log('mingi tegevus');
-    console.log('mingi tegevus');
-    console.log('mingi tegevus');
-console.timeEnd('test');
+// const - konstantne ehk pole võimalik muuta aga on võimalik lisada
+const minuEesNimi = 'Marleen';
+console.log(minuEesNimi);
+// minuEesNimi = 'Katrin'; konstanti muuta ei saa
 
-// kustutamine
-console.clear();
+// contant kasutamine
+const inimene = { nimi : 'Katrin', vanus : 20};
+console.table(inimene);
+inimene.vanus = 21;
+console.table(inimene);
 
-
+// const kasutamine 2
+const arvud = [1, 2, 3, 4, 5];
+console.log(arvud);
+arvud.push(6);
+console.log(arvud);
