418802fe2d9797413018dbbcbdde5fe14038d5ec
marleen kovalik <marleen.kovalik@khk.ee>
2019-10-28 10:54:02 +0200

    for tsükkel, 1-10 + paaritu või paaris

--- app.js
+++ app.js
@@ -2,11 +2,19 @@
 
 // for tsükkel
 // üldtsükkel
-let arv;
-for(arv = 1; arv <= 10; arv++){
+let lause;
+for(let arv = 1; arv <= 10; arv++){
+    lause = 'arv = ';
     if(arv < 10){
-        console.log('arv =  ' + arv);
-    } else {
-        console.log('arv = ' + arv); 
+        lause = lause + ' ';
+        
     } 
+    lause = lause + arv;
+    if(arv % 2 == 0){
+        lause = lause + ' -> paaris';
+    } else {
+        lause = lause + ' -> paaritu';
+    }
+    console.log(lause);
 }
+
