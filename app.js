1b49908a05f7257c5807008eaeb4af8884715fe5
marleen kovalik <marleen.kovalik@khk.ee>
2019-10-28 11:47:41 +0200

    jagaja

--- app.js
+++ app.js
@@ -2,11 +2,16 @@
 
 // while
 // eelkontrolliga tsükkel
-let number = 1; // defineerime tsükli juhtimismuutuja
-// paneme kontrolltingimuse paika
-while(number <= 10){
-    // sooritame tegevused
-    console.log(number);
-    // suurendame juhtimismuutuja väärtus
-    number++;
+let arv = 5; // defineerime numbri
+// defineerime jgaja algväätuse
+let jagaja = 2;
+while(arv % jagaja != 0){
+    
+    jagaja++;
+}
+
+if(arv == jagaja){
+    console.log(arv + ' on algarv');
+} else {
+    console.log(arv + ' ei ole algarv');
 }
