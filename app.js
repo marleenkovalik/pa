b671b83858637c46166578ccc86636c32f874beb
marleen kovalik <marleen.kovalik@khk.ee>
2019-10-28 11:03:02 +0200

    continue

--- app.js
+++ app.js
@@ -6,14 +6,15 @@ let lause;
 for(let arv = 1; arv <= 10; arv++){
     lause = 'arv = ';
     if(arv < 10){
-        lause = lause + ' ';
-        
+        lause = lause + ' ';   
     } 
     lause = lause + arv;
     if(arv % 2 == 0){
         lause = lause + ' -> paaris';
+        // continue;
     } else {
         lause = lause + ' -> paaritu';
+        continue;
     }
     console.log(lause);
 }
