23468a764105807a6258a25b4b007c9bc12abe6c
marleen kovalik <marleen.kovalik@khk.ee>
2019-10-28 11:33:47 +0200

    käsitsi nr sort

--- app.js
+++ app.js
@@ -2,8 +2,28 @@
 
 // for tsükkel
 // üldtsükkel
-let autod = ['Kia', 'Toyota', 'Ford', 'Nissan'];
+let numbrid = [];
 // väljastus
-for(let i = 0; i < autod.length; i++){
-    console.log(autod[i]);
+for(let i = 0; i < 10; i++){
+    let arv = Math.floor(Math.random() * 10);
+    numbrid.push(arv);
 }
+
+// kontroll
+console.log(numbrid);
+
+// sorteerime kasvujärjekorras
+//numbrid.sort();
+for(let kord = 0; kord < numbrid.length; kord++){
+for(let i = 0; i < numbrid.length; i++){
+    let abi;
+    if(numbrid[i] > numbrid[i+1]){
+        abi = numbrid[i];
+        numbrid[i] = numbrid[i+1];
+        numbrid[i+1] = abi;
+    }
+    console.log(numbrid);
+}
+}
+// kontrollime tulemust
+console.log(numbrid);
