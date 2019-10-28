6d9a4f82bca93ea5455999989c57244233564e8e
marleen kovalik <marleen.kovalik@khk.ee>
2019-10-28 11:11:58 +0200

    for nimekiri

--- app.js
+++ app.js
@@ -2,23 +2,8 @@
 
 // for tsükkel
 // üldtsükkel
-let lause;
-for(let arv = 1; arv <= 10; arv++){
-    lause = 'arv = ';
-    if(arv < 10){
-        lause = lause + ' ';   
-    } 
-    lause = lause + arv;
-    if(arv % 2 == 0){
-        lause = lause + ' -> paaris';
-        
-    } else {
-        lause = lause + ' -> paaritu';
-    
-    }
-    console.log(lause);
-    if(arv >= 5) {
-        break;
-    }
+let autod = ['Kia', 'Toyota', 'Ford', 'Nissan'];
+// väljastus
+for(let i = 0; i < autod.length; i++){
+    console.log(autod[i]);
 }
-
